3db675de6037028f40f07be35d2bffbae9ed0e9c
c20245709lozano-debug <20245709.lozano@csav.edu.ph>
2026-09-04 16:29:10 +0700

    addEventListener

--- script.js
+++ script.js
@@ -32,12 +32,37 @@ heading.style.background = "yellow";
 heading.style.fontSize = "100px"; 
 console.log(heading.textContent);
 
+heading.addEventListener("click", function(){
+   heading.style.color = "red";
+   console.log ("Heading was clicked!");
+
+});
+
 const contactHeading = document.querySelector("#contact h2");
 console.log(contactHeading)
 
 const projectsHeading = document.querySelector ("#services h2");
 console.log(projectsHeading);
 
+const toggleButton = document.querySelector ('#switch');
+const body = document.querySelector('body');
+let isOff =false; 
 
 
+toggleButton.addEventListener ("click", function (){
+   isOff = !isOff;
+   if (isOff) {
+      body.style.backgroundColor = "black";
+      body.style.color = "white"; 
+
+   }else {
+      body.style.backgroundColor = "white";
+      body.style.color = "black"; 
+
+   }
+   
+
+
+
+});
 
